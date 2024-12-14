2f461029788496c721703ed2108b94570cb8f9e7
EzzEldeen <elkordydeveloper2003@gmail.com>
2024-12-14 23:31:28 +0200

    Grades

---
+++ Grades/src/StudentTest.java
@@ -0,0 +1,9 @@
+public class StudentTest {
+
+    @Test
+    public void testCalculateGrade() {
+        Student student = new Student("Ahmed", 82.0);
+        String grade = student.calculateGrade();
+        assertEquals("B", grade, "Grade should be B");
+    }
+}
